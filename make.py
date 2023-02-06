f4eb2a8c64d9e0a5032c088c7b97f85d888206be
Peter Debevec <peterusdebevec@gmail.com>
2023-02-06 10:56:55 +0100

    ...

--- make.py
+++ make.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import dask.dataframe as df
 import dask
-import lib as lb
+from lib import ml
 import numpy as np
 import pickle
 from scipy.sparse import csr_matrix, save_npz
@@ -9,18 +9,18 @@ from scipy.sparse import csr_matrix, save_npz
 file = pd.read_csv('data.csv')
 print(file.info())
 
-""" file.NIR_GS_sig2 = lb.to_array(file.NIR_GS_sig2)
-file.NIR_GS_sig20 = lb.to_array(file.NIR_GS_sig20)
-file.NIR_GS_sig2_G_corr = lb.to_array(file.NIR_GS_sig2_G_corr)
-file.NIR_GS_sig20_G_corr = lb.to_array(file.NIR_GS_sig20_G_corr) """
+""" file.NIR_GS_sig2 = ml.to_array(file.NIR_GS_sig2)
+file.NIR_GS_sig20 = ml.to_array(file.NIR_GS_sig20)
+file.NIR_GS_sig2_G_corr = ml.to_array(file.NIR_GS_sig2_G_corr)
+file.NIR_GS_sig20_G_corr = ml.to_array(file.NIR_GS_sig20_G_corr) """
 
 npy = file.to_numpy()
 npy = npy.reshape(1214, 11)
 
-npy[:, 4] = lb.to_array(npy[:, 4])
-npy[:, 6] = lb.to_array(npy[:, 6])
-npy[:, 7] = lb.to_array(npy[:, 7])
-npy[:, 8] = lb.to_array(npy[:, 8])
+npy[:, 4] = ml.to_array(npy[:, 4])
+npy[:, 6] = ml.to_array(npy[:, 6])
+npy[:, 7] = ml.to_array(npy[:, 7])
+npy[:, 8] = ml.to_array(npy[:, 8])
 
 np.save(open('data.npy', 'wb'), npy)
 
--- train.py
+++ train.py
@@ -1,9 +1,9 @@
 import pickle
-import lib as lb
+from lib import ml
 from sklearn.cluster import KMeans, SpectralClustering, MiniBatchKMeans
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
 
-x_train,x_test, y_train,y_test = lb.get_xy_data()
+x_train,x_test, y_train,y_test = ml.get_xy_data()
 x_train = x_train/255
 x_test = x_test/255
